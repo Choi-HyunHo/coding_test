4ce4d05a1e8b2b64e808a4ce79c079511f801bff
최현호 <x7788@naver.com>
2022-12-09 21:05:11 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    var answer = num_list.reverse();
+    return answer;
+}
+
+solution([1, 2, 3, 4, 5]);
+solution([1, 1, 1, 1, 1, 2]);
+solution([1, 0, 1, 1, 1, 3, 5]);
+
