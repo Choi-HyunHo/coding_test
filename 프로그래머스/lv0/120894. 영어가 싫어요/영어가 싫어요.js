498575bc05f3d08ff4a6f875bc5dae732383f1e3
최현호 <x7788@naver.com>
2023-04-26 09:48:22 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    let numStr = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
+
+    numStr.forEach((item, index) => {
+        numbers = numbers.replaceAll(item, index)
+    })
+
+    
+    return Number(numbers);
+}
