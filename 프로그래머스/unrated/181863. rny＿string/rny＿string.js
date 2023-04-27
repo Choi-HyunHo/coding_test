fb320e051e4a3b8e7b56ae5e0d757527b92191c1
최현호 <x7788@naver.com>
2023-04-27 15:53:30 +0900

    [unrated] Title: rny_string, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.js
@@ -0,0 +1,15 @@
+function solution(rny_string) {
+    var answer = '';
+    let arr = rny_string.split('')
+    
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === 'm'){
+            arr[i] = 'rn'
+        } else{
+            arr
+        }
+    }
+    
+    
+    return arr.join('');
+}
