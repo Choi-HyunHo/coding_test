bdfc99b40e0f28394102d51709071a50af76325b
최현호 <x7788@naver.com>
2023-04-30 01:17:09 +0900

    [unrated] Title: A 강조하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,10 @@
+function solution(myString) {
+    var answer = myString.split('').map((i) => {
+        if(i === 'a' | i === 'A'){
+            return 'A'
+        } else {
+            return i.toLowerCase()
+        }
+    })
+    return answer.join('');
+}
