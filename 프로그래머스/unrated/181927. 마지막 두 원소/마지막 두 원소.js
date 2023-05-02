6b894ac505901a98deb72ce0a66ae4bee370d1a0
최현호 <x7788@naver.com>
2023-05-02 09:32:36 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    var answer = [];
+    
+    if(num_list[num_list.length-1] > num_list[num_list.length-2]){
+        return answer = [...num_list, num_list[num_list.length-1] - num_list[num_list.length-2]] 
+    } else if(num_list[num_list.length-1] <= num_list[num_list.length-2]){
+        return answer = [...num_list, num_list[num_list.length-1]*2] 
+    } else {
+        return answer;
+    }
+}
