81a61b69d243c942157b60a1ca5f8e3b9a6a2964
최현호 <x7788@naver.com>
2023-05-06 01:00:02 +0900

    [unrated] Title: 5명씩, Time: 3.36 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.js
@@ -0,0 +1,11 @@
+function solution(names) {
+    const result = [];
+    
+    for (let i = 0; i < names.length; i += 5) {
+        const group = names.slice(i, i + 5);
+        console.log(group[0])
+        result.push(group[0]);
+    }
+  
+    return result;
+}
