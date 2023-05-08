b3ad276c8a5310a9b70107581579c437e22c669f
최현호 <x7788@naver.com>
2023-05-08 09:26:12 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -0,0 +1,15 @@
+function solution(n_str) {
+    var answer = String(Number(n_str));
+    
+//     for(let i = 0; i < answer.length; i++){
+//         if(answer[i] === '0'){
+//             console.log(i)
+//             answer.shift();
+
+//         } else {
+//             break;
+//         }
+//     }
+    
+    return answer;
+}
